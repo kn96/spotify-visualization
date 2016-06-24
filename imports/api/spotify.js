5d21cd16521289d2161cda10542c685f0b43ec70
Snyder <ksnyder2@GA.AFGINC.COM>
2016-06-24 14:57:29 -0400

    Added listing of all tracks Place spotify-web-api.js in C:\Users\<user>\AppData\.meteor\packages\xinranxiao_spotify-web-api\1.0.2\npm\node_modules\spotify-web-api-node\src\

--- imports/api/spotify.js
+++ imports/api/spotify.js
@@ -11,12 +11,38 @@ Meteor.methods({
   getPlaylist: function (playlistId) {
     var spotifyApi = new SpotifyWebApi()
     var response = spotifyApi.getPlaylist(Meteor.user().services.spotify.id, playlistId, {})
-    console.log(playlistId)
 
     if (checkTokenRefreshed(response, spotifyApi)) {
       var response = spotifyApi.getPlaylist(Meteor.user().services.spotify.id, playlistId, {})
     }
     return response.data.body
+  },
+  getPlaylistTracks: function (playlistId) {
+    var spotifyApi = new SpotifyWebApi()
+    var response = spotifyApi.getPlaylistTracks(Meteor.user().services.spotify.id, playlistId, {})
+    if (checkTokenRefreshed(response, spotifyApi)) {
+      var response = spotifyApi.getPlaylistTracks(Meteor.user().services.spotify.id, playlistId, {})
+    }
+    return response.data
+  },
+  getUser: function () {
+    var spotifyApi = new SpotifyWebApi()
+    var response = spotifyApi.getUser(Meteor.user().services.spotify.id)
+    if (checkTokenRefreshed(response, spotifyApi)) {
+      var response = spotifyApi.getUser(Meteor.user().services.spotify.id)
+    }
+    return response.data
+  },
+  getArtists: function () {
+    var spotifyApi = new SpotifyWebApi()
+    var response = spotifyApi.getPlaylistTracks(Meteor.user().services.spotify.id, playlistId, 
+      {"fields": "items(track(artists(external_urls(spotify),name)))"})
+    if (checkTokenRefreshed(response, spotifyApi)) {
+      var response = spotifyApi.getPlaylistTracks(Meteor.user().services.spotify.id, playlistId, 
+        {"fields": "items(track(artists(external_urls(spotify),name)))"})
+    }
+    console.log(response.data)
+    return response.data
   }
 })
 
--- imports/ui/spotifyConfig.js
+++ imports/ui/spotifyConfig.js
@@ -1,3 +1,7 @@
+/*
+ * Client configuration for songs table view
+ */
+
 Template.spotify.onCreated( function spotifyOnCreated() {
   console.log('Created!')
   this.dict = new ReactiveDict()
@@ -8,21 +12,20 @@ Template.spotify.onCreated( function spotifyOnCreated() {
   })
 })
 
+Template.user.onCreated( function userOnCreated () {
+  this.dict = new ReactiveDict()
+  const instance = Template.instance()
+  Meteor.call('getUser', function (error, result) {
+    console.log('Retrieved user data')
+    instance.dict.set('userProfile', result.body)
+  })
+})
+
 Template.spotify.onRendered( function spotifyOnRendered() {
 })
 
 
 Template.spotify.helpers({
-  display_name() {
-    if (Meteor.user()) {
-      return Meteor.user().profile.display_name
-    }
-  },
-  id() {
-    if (Meteor.user()) {
-      return Meteor.user().profile.id
-    }
-  },
   hasCurrentPlaylist() {
     if (Template.instance().dict.get('currentPlaylist')) {
       console.log('There\'s a new playlist')
@@ -33,16 +36,28 @@ Template.spotify.helpers({
     }
   },
   playlists() {
+    console.log(Template.instance().dict.get('playlists'))
     return Template.instance().dict.get('playlists')
   },
   playlistName() {
-    return Template.instance().dict.get('currentPlaylist').name
+    if (Template.instance().dict.get('currentPlaylist') !== undefined) {
+      return Template.instance().dict.get('currentPlaylist').name
+    }
   },
-  mediumImage() {
-    return Template.instance().dict.get('currentPlaylist').images[1].url
+  largeImage() {
+    if (Template.instance().dict.get('currentPlaylist') !== undefined) {
+      return Template.instance().dict.get('currentPlaylist').images[0].url
+    }
   },
   songs() {
-    return Template.instance().dict.get('currentPlaylist').tracks.items
+    if (Template.instance().dict.get('currentPlaylistTracks') !== undefined) {
+      return Template.instance().dict.get('currentPlaylistTracks')
+    }
+  },
+  songCount() {
+     if (Template.instance().dict.get('currentPlaylistTracks') !== undefined) {
+      return Template.instance().dict.get('currentPlaylistTracks').length
+    }
   }
 })
 
@@ -51,6 +66,11 @@ Template.spotify.events({
     console.log('changing playlist')
     Meteor.call('getPlaylist', event.target.id, function (error, result) {
       instance.dict.set('currentPlaylist', result)
+      instance.dict.set('currentPlaylistTracks', [])
+    })
+    Meteor.call('getPlaylistTracks', event.target.id, function (error, result) {
+      instance.dict.set('currentPlaylistTracks', result)
+      console.log(result)
     })
   }
 })
@@ -68,5 +88,41 @@ Template.song.helpers({
       allArtists = allArtists + this.track.artists[j].name
     }
     return allArtists
+  }
+})
+
+Template.user.helpers({
+  display_name() {
+    if (Meteor.user()) {
+      return Meteor.user().profile.display_name
+    }
+  },
+  id() {
+    if (Meteor.user()) {
+      return Meteor.user().profile.id
+    }
+  },
+  profile_picture() {
+    if (Template.instance().dict.get('userProfile')) {
+      return Template.instance().dict.get('userProfile').images[0].url
+    }
+  },
+  followers() {
+    if (Template.instance().dict.get('userProfile')) {
+      return Template.instance().dict.get('userProfile').followers.total
+    }
+  }
+})
+
+Template.playlistSummary.helpers({
+  playlistImage(images) {
+    return images[0].url
+  },
+  no_access(playlistOwner, isPublic, isCollaborative) {
+    if (playlistOwner === Meteor.user().id || isPublic || isCollaborative) {
+      return false
+    } else {
+      return true
+    }
   }
 })
