ff074d6da89888f6105d877a37ceb5afa19c8d59
kn96 <knsnyder96@gmail.com>
2016-06-26 10:26:23 -0400

    Update index.js

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,8 +2,8 @@ ServiceConfiguration.configurations.update( /* Configure server with correct spo
 	{ "service": "spotify" },
 	{
 		$set: {
-			"clientId": "a1990ba28ee8492b919e9977721afd70",
-			"secret": "1606660678ba4e92bdfacac252181202"
+			"clientId": "",
+			"secret": ""
 		}
 	},
 	{ upsert: true }
